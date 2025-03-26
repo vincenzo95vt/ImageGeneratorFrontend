6f83ee6de60d8891eacbe1f4c6e550ada26aec62
Vinnie <pablovasta1995@gmail.com>
2025-03-27 00:57:44 +0100

    Subiendo cambios de ImGenerator

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import MainComponent from './components/MainComponent/MainComponent'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <MainComponent>
+      
+     </MainComponent>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MainComponent/MainComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './MainComponent.css'
+
+const MainComponent = () => {
+  return (
+    <div className='main-div'>
+      <h2 className='name'>Image Generator</h2>
+    </div>
+  )
+}
+
+export default MainComponent
